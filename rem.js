aa6fe3133dcccd61e565bfac8f8420aad400a376
xue wei <v.xuewei@shengqugames.com>
2019-11-14 18:43:13 +0800

    移动端测

---
+++ rem.js
@@ -0,0 +1,15 @@
+function windowResize() {
+    // console.log('width', window.innerWidth);
+    let width = window.innerWidth;
+    width = Math.max(width, 320)
+    width = Math.min(width, 750)
+
+    // document.querySelector('html').setAttribute('fontSize', width / 15 + 'px');
+    console.log('fontSize', width /15)
+    document.querySelector('html').style.fontSize = width /15 +'px';
+
+}
+// 给window绑定一个resize事件
+window.addEventListener('resize', windowResize);
+// 手动触发一次，相当于jQuery的trigger
+window.dispatchEvent(new Event('resize'))
