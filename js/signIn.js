9d75738acfef0ca90af255ea47fd110049dfff8d
SOMA_XUE <SOMA_XUE@outlook.com>
2019-11-17 22:16:04 +0800

    test5

--- js/signIn.js
+++ js/signIn.js
@@ -3,11 +3,13 @@ window.Zepto = window.$ = Zepto;
 Zepto(function($) {
   window.dispatchEvent(new Event("resize"));
 
-  console.log("Ready to Zepto!");
-  $('.signInBtn').on('tap', () => {
-    // $('.signInBtn').text(`${1++}`)
-    const btn = document.getElementsByClassName('signInBtn')
-    // btn.style.
+  // $('.signInBtn').on('tap', () => {
+  //   // $('.signInBtn').text(`${1++}`)
+  //   const btn = document.getElementsByClassName('signInBtn')
+  //   // btn.style.
+  //   $('.signInBtn').css('background', 'white')
+  // })
+  $('.signInBtn').click(() => {
     $('.signInBtn').css('background', 'white')
   })
 //   $('.signInBtn').on('touch', () => {
