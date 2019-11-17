3f6fbf0137607e0e7f5a42ac9bf9ff36f84ec92f
SOMA_XUE <SOMA_XUE@outlook.com>
2019-11-17 22:29:11 +0800

    touchJs

--- js/signIn.js
+++ js/signIn.js
@@ -1,6 +1,6 @@
 window.Zepto = window.$ = Zepto;
 
-Zepto(function($) {
+Zepto(function ($) {
   window.dispatchEvent(new Event("resize"));
 
   // $('.signInBtn').on('tap', () => {
@@ -9,12 +9,45 @@ Zepto(function($) {
   //   // btn.style.
   //   $('.signInBtn').css('background', 'white')
   // })
-  $('.signInBtn').click(() => {
+  $('.signInBtn').tap(() => {
     $('.signInBtn').css('background', 'white')
   })
-//   $('.signInBtn').on('touch', () => {
-//     $('.signInBtn').text(`${1++}`)
-//   })
+  //   $('.signInBtn').on('touch', () => {
+  //     $('.signInBtn').text(`${1++}`)
+  //   })
 
+  // 发送ajax请求, 进行登录
+  $.ajax({
+    type: "post",
+    url: "/user/login",
+    data: {
+      username: username,
+      password: password
+    },
+    dataType: "json",
+    success: function (info) {
+      console.log(info);
+      // 用户名或者密码错误
+      if (info.error) {
+        mui.toast("用户名或者密码错误");
+        return;
+      }
+      // 登录成功
+      if (info.success) {
+        if (location.search.indexOf("retUrl") > -1) {
+          // (1) 传了地址, 就跳转到对应页面
+          var retUrl = location.search.replace("?retUrl=", "");
+          location.href = retUrl;
+        }
+        else {
+          // (2) 没传地址, 跳转到个人中心页
+          location.href = "user.html";
+        }
+      }
+    },
+    error: function() {
+      console.log('erro ajax')
+    }
+  })
 
 });
