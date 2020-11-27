87e99ca830eabaa6fa443a75547ddc499e954518
xue wei <v.xuewei@shengqugames.com>
2020-11-27 18:39:17 +0800

    jfls

--- typescript/9.ts
+++ typescript/9.ts
@@ -1,4 +1,31 @@
-interface Person {
-    name: string;
-    age: number;
+// 接口的兼容性
+namespace aa {
+    interface Animal {
+        name: string;
+        age: number;
+    }
+    interface Person {
+        name: string;
+        age: number;
+        speak: () => void
+    }
+
+    let xiaoming: Person ={
+        name: 'xiaoming',
+        age:13,
+        speak: () => {
+            console.log('haha')
+        }
+    }
+
+    function conName(v: Animal) {
+        console.log(v.name);
+    }
+
+    conName(xiaoming);
+
+    // 函数兼容性
+    interface xx {
+        
+    }
 }
